4d55dbb040beed2575e4e522a74aec9f0d773e30
nickbuida <nickbui21@augustana.edu>
2024-09-25 10:24:15 -0500

    "Now with dancing chicken"

--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -1,7 +1,12 @@
 package edu.augustana;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello bird world!");
+        Chicken chicken = new Chicken();
+        chicken.dance();
     }
+
 }
+
